4718c3dc432a0983236a42ea7f4f26b979b33fc4
Delcari <delcari@protonmail.com>
2020-09-07 12:11:06 +1000

    ENHANCE: Removed Chess.JS Dependency

--- web/src/components/Game/WithMoveValidation.js
+++ web/src/components/Game/WithMoveValidation.js
@@ -1,162 +1,42 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import Chess from "chess.js"; // import Chess from  "chess.js"(default) if recieving an error about new Chess() not being a constructor
+import React from 'react';
 import $ from 'jquery'
 import Chessboard from "chessboardjsx";
 
-class HumanVsHuman extends Component {
-  static propTypes = { children: PropTypes.func };
-
-  state = {
-    fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-    // square styles for active drop square
-    dropSquareStyle: {},
-    // custom square styles
-    squareStyles: {},
-    // square with the currently clicked piece
-    pieceSquare: "",
-    // currently clicked square
-    square: "",
-    // array of past game moves
-    history: []
-  };
-
-  componentDidMount() {
-    this.game = new Chess();
-  }
-  
-  //Validate Movement, Update Board if Move is Valid
-  onDrop = ({ sourceSquare, targetSquare }) => {
-    // see if the move is legal
-    $.post(`${process.env.REACT_APP_API_URL}/move`, { FEN: this.state.fen, from: targetSquare, to: sourceSquare }).then(response => {
-      console.log(response)  
-    //break if nothing needs to be changed eg. illegal move   
-    if (response.valid !== 'Legal') {return}
-    this.setState(() => ({
-      //Update the board!
-      fen: response.FEN
-    }));
-  }
-)};  
+class Game extends React.Component {
+    constructor() 
+    {
+    super()
+    this.state = {
+        fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    }
+    } 
 
 
-
-
-
-
-
-
-  // keep clicked square style and remove hint squares
-  removeHighlightSquare = () => {
-    this.setState(({ pieceSquare, history }) => ({
-      squareStyles: squareStyling({ pieceSquare, history })
-    }));
-  };
-
-  // show possible moves
-  highlightSquare = (sourceSquare, squaresToHighlight) => {
-    const highlightStyles = [sourceSquare, ...squaresToHighlight].reduce(
-      (a, c) => {
-        return {
-          ...a,
-          ...{
-            [c]: {
-              background:
-                "radial-gradient(circle, #fffc00 36%, transparent 40%)",
-              borderRadius: "50%"
-            }
-          },
-          ...squareStyling({
-            history: this.state.history,
-            pieceSquare: this.state.pieceSquare
-          })
-        };
+    onDrop = ({ sourceSquare, targetSquare }) => {
+        // see if the move is legal
+        $.post(`${process.env.REACT_APP_API_URL}/move`, { FEN: this.state.fen, from: targetSquare, to: sourceSquare }).then(response => {
+          console.log(response)  
+        //break if nothing needs to be changed eg. illegal move   
+        if (response.valid !== 'Legal') {return}
+        this.setState(() => ({
+          //Update the board!
+          fen: response.FEN
+        }));
       }
-    );
+    )};  
 
-    this.setState(({ squareStyles }) => ({
-      squareStyles: { ...squareStyles, ...highlightStyles }
-    }));
-  };
-
-
-
-
-
-
-
-  onMouseOutSquare = square => this.removeHighlightSquare(square);
-
-  // central squares get diff dropSquareStyles
-  onDragOverSquare = square => {
-    this.setState({
-      dropSquareStyle:
-        square === "e4" || square === "d4" || square === "e5" || square === "d5"
-          ? { backgroundColor: "cornFlowerBlue" }
-          : { boxShadow: "inset 0 0 1px 4px rgb(255, 255, 0)" }
-    });
-  };
-
-
-  render() {
-    const { fen, dropSquareStyle, squareStyles } = this.state;
-
-    return this.props.children({
-      squareStyles,
-      position: fen,
-      // onMouseOverSquare: this.onMouseOverSquare,
-      onMouseOutSquare: this.onMouseOutSquare,
-      onDrop: this.onDrop,
-      dropSquareStyle,
-      onDragOverSquare: this.onDragOverSquare,
-    });
-  }
+    render()
+    {
+        return (
+            <div>
+                <Chessboard
+                position={this.state.fen}
+                onDrop={this.onDrop}
+                />
+            </div>
+        )
+    }   
 }
 
-export default function WithMoveValidation() {
-  return (
-    <div>
-      <HumanVsHuman>
-        {({
-          position,
-          onDrop,
-          //onMouseOverSquare,
-          onMouseOutSquare,
-          squareStyles,
-          dropSquareStyle,
-          onDragOverSquare
-        }) => (
-          <Chessboard
-            id="humanVsHuman"
-            position={position}
-            onDrop={onDrop}
-            // onMouseOverSquare={onMouseOverSquare}
-            onMouseOutSquare={onMouseOutSquare}
-            squareStyles={squareStyles}
-            dropSquareStyle={dropSquareStyle}
-            onDragOverSquare={onDragOverSquare}
-          />
-        )}
-      </HumanVsHuman>
-    </div>
-  );
-}
 
-const squareStyling = ({ pieceSquare, history }) => {
-  const sourceSquare = history.length && history[history.length - 1].from;
-  const targetSquare = history.length && history[history.length - 1].to;
-
-  return {
-    [pieceSquare]: { backgroundColor: "rgba(255, 255, 0, 0.4)" },
-    ...(history.length && {
-      [sourceSquare]: {
-        backgroundColor: "rgba(255, 255, 0, 0.4)"
-      }
-    }),
-    ...(history.length && {
-      [targetSquare]: {
-        backgroundColor: "rgba(255, 255, 0, 0.4)"
-      }
-    })
-  };
-};
+export default Game;
