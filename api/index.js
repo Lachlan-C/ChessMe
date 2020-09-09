d550e72a58c55d85dcb2025f4c2125cb38e1cd0b
Delcari <delcari@protonmail.com>
2020-09-09 21:48:47 +1000

    ENHANCE: Removed MQTT Dependency

--- api/index.js
+++ api/index.js
@@ -4,7 +4,6 @@ var stockfish = require('stockfish');
 const { Chess } = require('chess.js');
 var chessGame = new Chess();
 
-const mqtt = require('mqtt');
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -12,7 +11,7 @@ const bodyParser = require('body-parser');
 
 dotenv.config();
 
-const { MONGO_URL, PORT, BROKER, SUBSCRIBE_PATH, MOVE_VALID_URL } = process.env;
+const { MONGO_URL, PORT } = process.env;
 const app = express();
 
 mongoose.connect(MONGO_URL, {useNewUrlParser:true, useUnifiedTopology:true});
@@ -35,15 +34,16 @@ app.use(bodyParser.urlencoded({
 }));
 
 
-//MovesArray Example:
+//movesArray Example:
 // = [e2e3, a2b2, a1c3]
 //FEN is optional if not provided then the game starting pos is assumed
 function toFEN(movesArray, FEN)
 {
+    let chess
     if(FEN)
-        let chess = new Chess(FEN)
+        chess = new Chess(FEN)
     else
-        let chess = new Chess()
+        chess = new Chess()
 
     movesArray.map((movement) => 
         chess.move(movement, {sloppy: true})
@@ -79,59 +79,39 @@ engine.onmessage = (data) => {
 })
 //--------------------------------------------------
 
-const client = mqtt.connect(BROKER);
-
-
-
 //Move Validation Relocated
 function onDrop (source, target) {
     var move = chessGame.move({
-      from: source,
-      to: target,
-      promotion: 'q' 
+        from: source,
+        to: target,
+        promotion: 'q' 
     })
-  
+    
     if (move === null) {
-    return 'Illegal'
+        return 'Illegal'
     } else {
-    return 'Legal'
+        return 'Legal'
     }
-  }
-  
-  app.post('/validate/move', (req, res) => {
-      const {
-          to,
-          from,
-          FEN
-      } = req.body;
-      chessGame = new Chess(FEN);
-      res.send({valid: onDrop(to,from), FEN: chessGame.fen()});
-  });
+}
 
-
-app.post('/validate/move', (req,res)=> {
-
-    //Posts to External Device
-
-    const gameId = req.gameId;
-    const legality = req.move;
-
-    client.publish(`/${gameId}`, {move: legality}, () => {
-        res.send('Published move legality.'); //This can be taken out and is used for testing.
-    });
+app.post('/validate/move', (req, res) => {
+    const {
+        to,
+        from,
+        FEN
+    } = req.body;
+    chessGame = new Chess(FEN);
+    res.send({valid: onDrop(to,from), FEN: chessGame.fen()});
 });
 
-client.on('message', (topic, message) => {
-    console.log('received data!');
-    if (topic == SUBSCRIBE_PATH) {
-        gameId = message.gameId;
-        move = message.move;
-
-        app.put(`${MOVE_VALID_URL}/validate/move`, (req) => {
-
-        });
-    }
-})
+app.post('/chess/moves', (req, res) => {
+    const {
+        FEN,
+        piecePos
+    } = req.body;
+    chessGame = new Chess(FEN)
+    res.send(chessGame.moves({ square: piecePos }))
+});
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
