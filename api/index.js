fc8a79e114fecebd59c95d5c08736eee848df285
Delcari <delcari@protonmail.com>
2020-09-09 17:16:10 +1000

    NEW: StockFish Moves

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 
-// DISCUSS WHETHER TO MERGE WITH MOVE_VALIDATION: CHANGES DATA STRUCTURE/COMPLEXITY.
+var stockfish = require('stockfish');
+
 const { Chess } = require('chess.js');
 var chessGame = new Chess();
 
@@ -11,12 +12,7 @@ const bodyParser = require('body-parser');
 
 dotenv.config();
 
-const {MONGO_URL} = process.env;
-const {PORT} = process.env;
-const {BROKER} = process.env;
-const {SUBSCRIBE_PATH} = process.env;
-const {MOVE_VALID_URL} = process.env;
-
+const { MONGO_URL, PORT, BROKER, SUBSCRIBE_PATH, MOVE_VALID_URL } = process.env;
 const app = express();
 
 mongoose.connect(MONGO_URL, {useNewUrlParser:true, useUnifiedTopology:true});
@@ -31,14 +27,61 @@ app.use((req,res,next)=> {
     next();
 });
 
-const port = process.env.PORT || 5000;
+const port = PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+
+//MovesArray Example:
+// = [e2e3, a2b2, a1c3]
+//FEN is optional if not provided then the game starting pos is assumed
+function toFEN(movesArray, FEN)
+{
+    if(FEN)
+        let chess = new Chess(FEN)
+    else
+        let chess = new Chess()
+
+    movesArray.map((movement) => 
+        chess.move(movement, {sloppy: true})
+    )
+    return chess.fen();
+}
+
+
+//StockFISH ---------------------------------------
+var engine = stockfish();
+function send(str)
+{
+    console.log(`Sending: ${str}`)
+    engine.postMessage(str);
+}
+
+
+app.post('/stockfish/move', (req,res) => {
+const { FEN } = req.body 
+send(`position fen ${FEN}`)
+send('go')
+
+engine.onmessage = (data) => {
+        //Uses regex to find the best move
+        const move = data.match(/bestmove\s+(\S+)/)
+
+        //Finds the FEN string
+        if (move)
+        {
+            res.send(toFEN([move[1]], FEN))
+        }
+}
+})
+//--------------------------------------------------
+
 const client = mqtt.connect(BROKER);
+
+
 
 //Move Validation Relocated
 function onDrop (source, target) {
@@ -64,7 +107,8 @@ function onDrop (source, target) {
       chessGame = new Chess(FEN);
       res.send({valid: onDrop(to,from), FEN: chessGame.fen()});
   });
-  
+
+
 app.post('/validate/move', (req,res)=> {
 
     //Posts to External Device
