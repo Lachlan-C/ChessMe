4f40fef233b4cf70c18981c1c3339684f95d24d7
Lachlan <lachlan@fakeemail.com>
2020-09-16 22:57:13 +1000

    FIX: Database Time Date Storage

--- api/models/game.js
+++ api/models/game.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 module.exports = mongoose.model('Game', new mongoose.Schema({
     GameID: String,
-    DateTimeStart: String,
-    DateTimeEnd: String, 
+    DateTimeStart: Date,
+    DateTimeEnd: Date, 
     Users: Array,
     Moves: Array,
     CurrentFen: String,
