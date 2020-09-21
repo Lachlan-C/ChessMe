6ebea5775361d20fea48599b21c2db2d6e44851d
Delcari <delcari@protonmail.com>
2020-09-21 16:23:06 +1000

    ENHANCE: New JEST Endpoints

--- testing/api.test.js
+++ testing/api.test.js
@@ -14,6 +14,42 @@ test('/test /GET', () => {
     )
 })
 
+test('/game/:gameid/fen /GET', () => {
+    expect.assertions(1);
+    return fetch(`${API_URL}/game/12345678/fen`, {method: 'GET'})
+    .then(res => res.json())
+    .then(res => 
+        expect(res.FENs[0]).toEqual("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    )
+})
+
+test('/user/board/pair /POST', () => {
+    let body = {
+        userID: "00000001",
+        boardID: "1111"
+    }
+    expect.assertions(1);
+    return fetch(`${API_URL}/user/board/pair`, { method: 'POST', body: JSON.stringify(body), headers: { 'Content-Type': 'application/json' }} )
+    .then(res => res.json())
+    .then(res => 
+        expect(res[0].boardID).toEqual('1111')
+        )
+})
+
+test('/user/login /POST', () => {
+    let body = {
+        username: "Riley",
+        password: "testing"
+    }
+    expect.assertions(1);
+    return fetch(`${API_URL}/user/login`, { method: 'POST', body: JSON.stringify(body), headers: { 'Content-Type': 'application/json' }} )
+    .then(res => res.text())
+    .then(res => 
+        expect(res).toEqual("loggedIn")
+        )
+})
+
+
 test('/user/:userid/games /GET', () => {
     expect.assertions(1);
     return fetch(`${API_URL}/user/00000001/games`, {method: 'GET'})
@@ -32,13 +68,88 @@ test('/user/:username /GET', () => {
     )
 })
 
-test('/game/:gameid/fen /GET', () => {
+
+/*************************
+I DONT SEND ANY RESPONSE YET AFAIK
+***************************/
+// test('/user/register /POST', () => {
+//     let body = {
+//         username: "Magical",
+//         password: "jest"
+//     }
+//     expect.assertions(1);
+//     return fetch(`${API_URL}/user/register`, { method: 'POST', body: JSON.stringify(body), headers: { 'Content-Type': 'application/json' }} )
+//     .then(res => res.text())
+//     .then(res => 
+//         expect(res).toEqual("00000001")
+//         )
+// })
+
+test('/stockfish/move /POST', () => {
+    let body = {
+        GameID: "12345678",
+        FEN: "rnbqkbnr/pp1ppppp/8/2p5/5PK1/8/PPPPP1PP/RNBQ1BNR b kq - 0 1",
+        difficulty: 20
+    }
     expect.assertions(1);
-    return fetch(`${API_URL}/game/12345678/fen`, {method: 'GET'})
+    return fetch(`${API_URL}/stockfish/move`, { method: 'POST', body: JSON.stringify(body), headers: { 'Content-Type': 'application/json' }} )
+    .then(res => res.text())
+    .then(res => 
+        expect(res).toEqual('e7e5')
+        )
+})
+
+test('/validate/move /POST', () => {
+    let body = {
+        GameID: "12345678",
+        move: "f7f6",
+        UserID: "00000001"
+    }
+    expect.assertions(1);
+    return fetch(`${API_URL}/validate/move`, { method: 'POST', body: JSON.stringify(body), headers: { 'Content-Type': 'application/json' }} )
     .then(res => res.json())
     .then(res => 
-        expect(res.FENs[0]).toEqual("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-    )
+        expect(res.Users[0]).toEqual("00000001")
+        )
+})
+
+test('/chess/moves /POST', () => {
+    let body = {
+        GameID: "12345678",
+        piecePos: "g7"
+    }
+    expect.assertions(1);
+    return fetch(`${API_URL}/chess/moves`, { method: 'POST', body: JSON.stringify(body), headers: { 'Content-Type': 'application/json' }} )
+    .then(res => res.json())
+    .then(res => 
+        expect(res[0]).toEqual('g6')
+        )
+    })
+    
+// test('/chess/NewGame /POST', () => {
+//     let body = {
+//         PlayerID: "00000001",
+//         EnemyID: "00000003"
+//     }
+//     expect.assertions(1);
+//     return fetch(`${API_URL}/chess/NewGame`, { method: 'POST', body: JSON.stringify(body), headers: { 'Content-Type': 'application/json' }} )
+//     .then(res => res.text())
+//     .then(res => 
+//         expect(res).toHaveLength(8)
+//         )
+// })
+
+test('/chess/reconnect /POST', () => {
+    let body = {
+        username: "Riley",
+        gameID: "12345678"
+    }
+    expect.assertions(1);
+    return fetch(`${API_URL}/chess/reconnect`, { method: 'POST', body: JSON.stringify(body), headers: { 'Content-Type': 'application/json' }} )
+    .then(res => res.text())
+    .then(res => 
+        expect(res).toEqual("success")
+        )
 })
 
 test('/board/pair /POST', () => {
@@ -68,35 +179,8 @@ test('/game/connect /POST', () => {
         )
 })
 
-test('/stockfish/move /POST', () => {
-    let body = {
-        GameID: "12345678",
-        FEN: "rnbqkbnr/pp1ppppp/8/2p5/5PK1/8/PPPPP1PP/RNBQ1BNR b kq - 0 1",
-        difficulty: 20
-    }
-    expect.assertions(1);
-    return fetch(`${API_URL}/stockfish/move`, { method: 'POST', body: JSON.stringify(body), headers: { 'Content-Type': 'application/json' }} )
-    .then(res => res.text())
-    .then(res => 
-        expect(res).toEqual('e7e5')
-        )
-})
 
 
-test('/chess/moves /POST', () => {
-    let body = {
-        GameID: "12345678",
-        piecePos: "f2"
-    }
-    expect.assertions(1);
-    return fetch(`${API_URL}/chess/moves`, { method: 'POST', body: JSON.stringify(body), headers: { 'Content-Type': 'application/json' }} )
-    .then(res => res.json())
-    .then(res => 
-        expect(res[0]).toEqual('f3')
-        )
-})
 
 
-//newgame 
 
-//validate move
